e76ac0c7849716593b6433a4059833e62aa2361f
Jonathan Brannan <jbrannan@borncapital.com>
2012-05-01 11:30:40 -0400

    Eragy input takes watt hours and not raw watts. They also don't seem to like more than one access per minute or more than one data point per minute.  So we send one watt hour per device per minute and combine multiple serials together.

--- ecm/processors/eragy.py
+++ ecm/processors/eragy.py
@@ -60,7 +60,7 @@ CLASS='EragyProcessor'
 class Constants:
 # eragy defaults
   ERAGY_URL           = 'http://d.myeragy.com/energyremote.aspx'
-  ERAGY_UPLOAD_PERIOD = 15 * MINUTE
+  ERAGY_UPLOAD_PERIOD = 2 * MINUTE
   ERAGY_TIMEOUT       = 15 # seconds
   ERAGY_GATEWAY_ID    = ''
   ERAGY_TOKEN         = ''
@@ -90,14 +90,45 @@ class EragyProcessor(UploadProcessor):
             if not self.token:
                 print '  No token'
             sys.exit(1)
+        self.last_watts = {}
+        self.last_publish_times = {}
+        self.sns = {}
+    
+    def process_compiled(self, packet, packet_buffer):
+        sn = getserial(packet)
+        self.sns[sn] = True
+        if not self.time_to_upload(sn):
+            return
+        total_packets = {}
+        for sn in self.sns:
+          packets = []
+          data = packet_buffer.data_over(sn, self.upload_period)
+          dbgmsg('%d packets to process' % len(data))
+          for a,b in zip(data[0:], data[1:]):
+              packets.append(calculate(b[1],a[1]))
+          total_packets[sn] = packets
+        self.process_calculated(total_packets)
 
-    def process_calculated(self, ecm_serial, packets):
-        osn = obfuscate_serial(ecm_serial)
-        s = []
-        for p in packets:
-            for idx,c in enumerate(ECM1240_CHANNELS):
+    def process_calculated(self, total_packets):
+        time_samples = {}
+        times = {}
+        for ecm_serial, packets in total_packets.iteritems():
+          osn = obfuscate_serial(ecm_serial)
+          for p in packets:
+            if p['time_created'] - self.last_publish_times.get(ecm_serial, 0) >= 59:
+              for idx,c in enumerate(ECM1240_CHANNELS):
                 key = osn + '_' + c
-                s.append('<MTU ID="%s"><cumulative timestamp="%s" watts="%d"/></MTU>' % (key,p['time_created'],p[c+'_watts']))
+                if not key in times:
+                  times[key] = []
+                watts = p[c+'_wh'] * 1000
+  	        times[key].append('<cumulative timestamp="%s" watts="%d"/>' % (p['time_created'], watts))
+              self.last_publish_times[ecm_serial] = p['time_created']
+        s = []
+        for key, ts in times.iteritems():
+          s.append('<MTU ID="%s">' % (key))
+          for t in ts:
+            s.append(t)
+          s.append('</MTU>')
         if len(s):
             s.insert(0, '<ted5000 GWID="%s" auth="%s">' %
                      (self.gateway_id, self.token))
@@ -106,7 +137,8 @@ class EragyProcessor(UploadProcessor):
             resp = result.read()
             if not resp == '<xml>0</xml>':
                 wrnmsg('EG: upload failed for %s: %s' % (ecm_serial, resp))
-        self.mark_upload_complete(ecm_serial)
+        for ecm_serial in self.sns:
+          self.mark_upload_complete(ecm_serial)
 
     def _handle_urlopen_error(self, e, sn, url, payload):
         errmsg(''.join(['%s Error: %s' % (self.__class__.__name__, e),
